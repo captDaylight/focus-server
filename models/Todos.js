1a42d9aa361d86ae1246373829e8e3849c3414e8
paul <paul.christophe6@gmail.com>
2016-02-13 18:34:15 -0500

    posting a todo, fetching all todos

--- models/Todos.js
+++ models/Todos.js
@@ -1,16 +1,9 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-				// todo: action.todo,
-				// id: shortid.generate(),
-				// created: Date.now(),
-				// completed: null,
-				// workingOn: null, 
-				// editing: false,
-
 var TodosSchema = Schema({
 	todo: String,
-	id: String
+	id: String,
 	created: Date,
 	completed: Number,
 	workingOn: Number,
--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,45 @@
 var express = require('express');
 var router = express.Router();
+var Todo = require('../models/Todos');
 
 module.exports = function(passport) {
 	
 	router.get('/', function(req, res) {
 		res.json({message: 'OH THIS IS THE API'}); 
 	});
+
+	// TODOS
+	router.route('/todos')
+		.post(function (req, res) {
+			// create a todo
+			var todo = new Todo();
+			var body = req.body;
+
+			todo.todo = body.todo;
+			todo.id = body.id;
+			todo.created = body.created;
+			todo.completed = body.completed;
+			todo.workingOn = body.workingOn;
+			todo.editing = body.editing;
+
+			todo.save(function (err) {
+				if (err) return res.send(err);
+
+				return res.json({message: 'todo created'});
+			});
+		})
+		.get(function (req, res) {
+			Todo.find(function (err, todos) {
+				if (err) return res.send(err);
+
+				return res.json(todos);
+			});
+		});
+
+	router.route('/todos/:todo_id')
+		.get()
+		.put()
+		.delete();
 
 	return router;
 }
