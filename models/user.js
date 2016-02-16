d0f477444916a2c23629563bcc0619d9c94837c0
paul <paul.christophe6@gmail.com>
2016-02-16 18:34:01 -0500

    simplify user model

--- models/user.js
+++ models/user.js
@@ -1,10 +1,8 @@
 var mongoose = require('mongoose');
 
 module.exports = mongoose.model('User',{
-	id: String,
-	username: String,
-	password: String,
 	email: String,
-	firstName: String,
-	lastName: String
+	password: String,
+	name: String,
+	admin: Boolean,
 });
