d667015a2c40fa13bc6c32c232f98fe37baeb0eb
paul <paul.christophe6@gmail.com>
2016-04-14 21:30:32 -0400

    get chunk of data

--- routes/api.js
+++ routes/api.js
@@ -161,19 +161,19 @@ module.exports = function(db) {
 	});
 
 	//////////////////////////////
-	// ALL DATA
+	// GET ALL DATA
 	//////////////////////////////
 	router.route('/')
 		.get(function(req, res) {
 			var userEmail = req.decoded.email;
 			var users = db.collection('users');
-
+			console.log('hmm');
 			// get user
 			users.findOne({'email': userEmail}, function(err, user) {
-				return res.json({status: true, data: { user: user }});
+				// return res.json({status: true, data: { user: user }});
 				var ids = user.websiteIDs;
 				var websites = db.collection('websites');
-				
+				console.log('ok');
 				if (!user.websiteIDs) {
 					return res.json({status: true, data: { websites: [] }});
 				}
@@ -182,7 +182,20 @@ module.exports = function(db) {
 				websites.find({'_id': {'$in': ids}}).toArray(function(err, websites) {
 					if (err) return res.send(err);
 					
-					return res.json({status: true, data: { websites: websites }})
+					var morning = new Date();
+					morning.setHours(0, 0, 0, 0);
+
+					var morningUTC = morning.getTime();
+
+					var todaysSessions = user.sessions.filter(function (session) {
+						return session.start > morningUTC;
+					});
+
+					return res.json({status: true, data: { 
+						websites: websites, 
+						todos: user.todos,
+						sessions: todaysSessions
+					}})
 				});
 			});
 		});
